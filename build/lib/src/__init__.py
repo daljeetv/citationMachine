6e2d069408a05349ffba4a5167eb9f078f0d5a5e
Daljeet Virdi <daljeetv@gmail.com>
2016-02-19 18:23:07 -0800

    fix setup.py

---
+++ build/lib/src/__init__.py
@@ -0,0 +1,9 @@
+__author__ = 'daljeetv'
+import time
+
+start = time.time()
+print "hello"
+
+end = time.time()
+print "END"
+print end - start
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-import citation
+from src import citationmachine
 import os
 
 
